8de4e6a0175008303a121041d0d7908b8456fe31
BorislavRaynov <b_raynov@abv.bg>
2024-04-30 18:43:00 +0300

    asgi websocket urls set up

--- mouse_play/asgi.py
+++ mouse_play/asgi.py
@@ -8,7 +8,8 @@ https://docs.djangoproject.com/en/5.0/howto/deployment/asgi/
 """
 
 import os
-from channels.routing import ProtocolTypeRouter
+import mouse_observe.routing
+from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mouse_play.settings')
@@ -16,4 +17,7 @@ django_asgi_app = get_asgi_application()
 
 application = ProtocolTypeRouter({
     "http": django_asgi_app,
+    'websocket': URLRouter(
+            mouse_observe.routing.websocket_urlpatterns
+    ),
 })
