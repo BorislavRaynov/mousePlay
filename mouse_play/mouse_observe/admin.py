dffbd63d56b47cdd31edaa35913e2b200fae615d
BorislavRaynov <b_raynov@abv.bg>
2024-05-01 16:45:46 +0300

    Containerization of the app.

--- mouse_play/mouse_observe/admin.py
+++ mouse_play/mouse_observe/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import MouseClick
 
-# Register your models here.
+
+@admin.register(MouseClick)
+class MouseClickAdmin(admin.ModelAdmin):
+    list_display = ('x', 'y', 'image', 'created_at')
